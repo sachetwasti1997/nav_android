b66938f23ff73c484b18bf1ddbf1bc3e23001d20
sachetwasti1997 <sachetwasti61@gmail.com>
2022-04-30 21:34:32 +0530

    Initialised the whole app

---
+++ app/src/main/java/com/sachet/signupflow/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.sachet.signupflow
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.sachet.signupflow.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+    }
+}
